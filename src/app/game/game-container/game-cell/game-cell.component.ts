49175662f2e1e05e3bb3e8b825f8067e4bc519b5
Andy Radulescu <andyradulescu@Andys-MacBook-Pro.local>
2019-07-10 14:09:13 +0200

    basic functionality in place

--- src/app/game/game-container/game-cell/game-cell.component.ts
+++ src/app/game/game-container/game-cell/game-cell.component.ts
@@ -3,6 +3,8 @@ import {EmitterService} from '@ngxs-labs/emitter';
 import {GameState} from '../../core/game.state';
 import {Observable} from 'rxjs';
 import {Select} from '@ngxs/store';
+import {Game} from '../../core/models';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-game-cell',
@@ -11,9 +13,15 @@ import {Select} from '@ngxs/store';
 })
 export class GameCellComponent implements OnInit {
 
+  static XorO = {
+    true: 'X',
+    false: '0'
+  };
   @Input() cellName: string;
   @Select(GameState)
-  public counter$: Observable<number>;
+  public counter$: Observable<Game>;
+  public cellValue: string;
+
 
   constructor(public emitter: EmitterService) {
   }
@@ -21,7 +29,14 @@ export class GameCellComponent implements OnInit {
   ngOnInit() {
   }
 
-  public move() {
+  public move(): void {
+    if (this.cellValue !== undefined) {
+      return;
+    }
+    this.counter$.pipe(take(1))
+      .subscribe((data) => {
+        this.cellValue = GameCellComponent.XorO[data.xTurn.toString()];
+      });
     this.emitter.action(GameState.addMovement).emit(this.cellName);
   }
 }
