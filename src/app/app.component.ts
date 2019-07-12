75a8de047eac7fdc71b85125b969cc1409760ed6
Andy Radulescu <eduard-andrei.radulescu@gmail.com>
2019-07-12 15:33:14 +0200

    redirect if console is open

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'XandZero';
+
+  ngOnInit(): void {
+    console.log('%c Stop!!!', 'background: #222; color: red; font-size:30px');
+  }
 }
--- src/app/game/game-container/game-container.component.ts
+++ src/app/game/game-container/game-container.component.ts
@@ -1,11 +1,15 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GameState} from '../core/game.state';
 import {Select} from '@ngxs/store';
-import {Observable, Subject, Subscription} from 'rxjs';
+import {fromEvent, Observable, Subscription, timer} from 'rxjs';
 import {Game} from '../core/models';
 import {CellPosition, FinishLinePosition} from '../interfaces/interfaces';
 import FinishGame from '../utils/finish-game';
 import {EmitterService} from '@ngxs-labs/emitter';
+import * as devTools from 'devtools-detect';
+import {take} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-game-container',
@@ -19,8 +23,10 @@ export class GameContainerComponent implements OnInit, OnDestroy {
   @Select(GameState)
   public gameState$: Observable<Game>;
   private subscription: Subscription;
+  private timerSubscription: Subscription;
+  private consoleSubscription: Subscription;
 
-  constructor(private emitter: EmitterService) {
+  constructor(private emitter: EmitterService, private router: Router) {
     this.cells = [];
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
@@ -30,6 +36,20 @@ export class GameContainerComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    const isOpen = devTools.isOpen;
+    this.consoleSubscription = fromEvent(window, 'devtoolschange').subscribe(data => {
+      if ((data as devTools.DevToolsEvent).detail.isOpen === false) {
+        if (this.timerSubscription) {
+          this.timerSubscription.unsubscribe();
+        }
+      } else {
+        console.log('%c YOU WILL BE REDIRECTED TO LOGIN IN 5 SECONDS!', 'background: #222; color: red; font-size:30px');
+        this.timerSubscription = timer(5000).pipe(take(1)).subscribe(next => {
+          this.router.navigate(['/home']);
+        });
+      }
+    });
+
     const finish = new FinishGame();
     this.subscription = this.gameState$.subscribe(data => {
       const finishLine = finish.verifyFinish(data.movementArray);
@@ -43,6 +63,7 @@ export class GameContainerComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
+    this.consoleSubscription.unsubscribe();
     this.subscription.unsubscribe();
   }
 }
